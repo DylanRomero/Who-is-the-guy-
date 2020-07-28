99e83e5b457e8ffc45dd18ed8f3aa6e09e42f6aa
Dylan Romero <dylanromero639@gmail.com>
2020-07-28 16:18:46 -0400

    added a commentary to remember whats next

--- who.py
+++ who.py
@@ -1,36 +1,39 @@
 def run():
-    nombres = ["juana", "carlos", "juxy", "hernesto", "ana", "cristiancito", "cacique", "jorge","chavez", "pepe"]
-    nombres_positivos = []
+    # the plan is to establish a document.py for all the functions of the program and then create another in wich we will import the functions to simplify
 
+    nombres = ["juana", "carlos", "juxy", "hernesto", "ana",
+               "cristiancito", "cacique", "jorge", "chavez", "pepe"]
+    nombres_positivos = []
 
     def calculo_vocales():
         for i in nombres:
             for letras in i:
                 diccionario = {
-                "a" : operacion_por_vocal(i, "a"),
-                "e" : operacion_por_vocal(i, "e"),
-                "i" : operacion_por_vocal(i, "i"),
-                "o" : operacion_por_vocal(i, "o"),
-                "u" : operacion_por_vocal(i, "u")
+                    "a": operacion_por_vocal(i, "a"),
+                    "e": operacion_por_vocal(i, "e"),
+                    "i": operacion_por_vocal(i, "i"),
+                    "o": operacion_por_vocal(i, "o"),
+                    "u": operacion_por_vocal(i, "u")
                 }
 
             if sum(diccionario.values()) == 2:
-                print(diccionario, i, "_____", "\033[1;32m"+"POSITIVO" + "\033[0;m" + " >>>> tiene 2 vocales")
+                print(diccionario, i, "_____",
+                      "\033[1;32m"+"POSITIVO" + "\033[0;m" + " >>>> tiene 2 vocales")
                 nombres_positivos.append(i)
             else:
-                print(diccionario, i, "_____" , "\033[1;31m" + "NEGATIVO" + "\033[0;m")
-        
+                print(diccionario, i, "_____",
+                      "\033[1;31m" + "NEGATIVO" + "\033[0;m")
+
         print("""
         Depurando ... 
         
         nombres con 2 vocales: 
          """)
-        
+
         for i in nombres_positivos:
             print(i)
 
-
-    def operacion_por_vocal(nombre, vocal): 
+    def operacion_por_vocal(nombre, vocal):
         cuenta = 0
         for letra in nombre:
             if letra == vocal:
